b65c4ca6a367d3db3364cb5ea9337e318e212128
Muzammal Rafique <muzammalrafique26@gmail.com>
2025-07-18 11:52:05 +0500

    lecture no 2

---
+++ lecture2.py
@@ -0,0 +1,24 @@
+# name = input("Enter Your Name: ")
+
+# age = input("Enter Your Age: ")
+
+# city = input ("Enter your City: ")
+
+# # Hello Ali! You are 22 years old and live in Lahore.
+
+# print(f"Hello {name}! You are {age} years old and live in {city}.")
+
+number1 = input("Enter Number 1: ")
+number2 = input("Enter Number 2: ")
+
+number1 = float(number1)
+number2 = float(number2)
+
+sum = number1 + number2
+diff = number1 - number2
+prod= number1 * number2
+div = number1 / number2
+print(f"Sum is {sum}")
+print(f"Difference is {diff}")
+print(f"Product is {prod}")
+print(f"Division is {div}")
