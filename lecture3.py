973f119f826dcdb7eb2c4d89a835f7d0c25fa2c3
Muzammal Rafique <muzammalrafique26@gmail.com>
2025-07-18 11:52:21 +0500

    lecture no 4

---
+++ lecture3.py
@@ -0,0 +1,13 @@
+# name = input("Enter Your Name: ")
+# print(name)
+# print(name.upper())
+# print(name[0:3])
+# print(len(name))
+
+countries = ["Pakistan", "India" ,"UAE" , "USA"]
+print(countries[1])
+print(countries[-1])
+countries.append("Chine")
+print(countries)
+countries.sort()
+print(countries)
