776baf475696a871f8504f3fa9fb9abb82bbdaff
Muzammal Rafique <muzammalrafique26@gmail.com>
2025-07-18 11:51:54 +0500

    lecture no 1

---
+++ lecture1.py
@@ -0,0 +1,22 @@
+# This is a comment
+
+print("Welcome to Python!")
+
+# Variables
+name = "Ali"
+age = 22
+height = 5.8
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+# Using f-strings
+print(f"{name} is {age} years old and {height} feet tall.")
+
+my_name = "Muzamil"
+my_age = 25
+my_city="Faisalabad"
+
+print(f"My name is {my_name}, I am {my_age} years old, and i live in {my_city}")
