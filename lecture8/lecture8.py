7426150c5d6dfd04a5441dd63d48b422695c3046
Muzammal Rafique <muzammalrafique26@gmail.com>
2025-07-21 12:53:13 +0500

    lecture 8

---
+++ lecture8/lecture8.py
@@ -0,0 +1,33 @@
+# import os
+# script_dir = os.path.dirname(__file__)
+# file_path = os.path.join(script_dir, "students.txt")
+# new_student = input("Enter the name of the new student: ")
+# with open(file_path, "a") as file:
+#     file.write("\n"+new_student)
+# print(f"{new_student} has been added to the students list.")
+
+file = open("marks.txt", "w")
+
+file.write("95\n")
+file.write("65\n")
+file.write("78\n")
+file.write("87\n")
+file.write("82\n")
+
+file.close()
+
+file = open("marks.txt", "r")
+total_marks = 0
+length = 0
+for line in file:
+    total_marks += int(line.strip())
+    length += 1
+file.close()
+average_marks = total_marks / length
+print(f"The total marks are: {total_marks}")
+print(f"The average marks are: {average_marks}")
+
+file = open("marks.txt", "a")
+file.write("Total Marks: " + str(total_marks) + "\n")
+file.write("Average Marks: " + str(average_marks) + "\n")
+file.close()
