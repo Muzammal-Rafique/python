0616fdc82a8f67adef88d93cc9e9c73b26eb7906
Muzammal Rafique <muzammalrafique26@gmail.com>
2025-07-21 11:41:42 +0500

    lecture 7 (dictionary and module)

---
+++ lecture7.py
@@ -0,0 +1,32 @@
+import math
+import my_utils
+
+# student = {
+#     "Name": "Ali",
+#     "Age": 18,
+#     "Marks": {
+#         "Math": 85,
+#         "Physics": 90,
+#         "English": 78
+#     }
+# }
+
+# print(student["Name"])
+# print(student["Age"])
+
+# print("\nSubjects and Marks:")
+# for subject, mark in student["Marks"].items():
+#     print(f"{subject}: {mark}")
+
+# marks = student["Marks"].values() 
+# average = sum(marks) / len(marks)
+
+# print(f"\nAverage Marks: {average:.2f}")
+
+# sqrt_average = math.sqrt(average)
+# print(f"Square Root of Average Marks: {sqrt_average:.2f}")
+
+# print(f"\nValue of Pi: {math.pi}")
+
+my_utils.welcome_message("Python Course")
+my_utils.greet("Muzammil")
---
+++ my_utils.py
@@ -0,0 +1,5 @@
+def welcome_message(name):
+    print(f"Welcome to the {name}!")
+
+def greet(name):
+    print(f"Hello, {name}!")
